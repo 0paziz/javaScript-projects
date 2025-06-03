3960567299fefbffda40db92b164ee00104a7500
Abdiaziz <145577421+0paziz@users.noreply.github.com>
2025-06-03 15:47:55 +0300

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 
 <>
 
-<footer className="w-full bg-gray-100 text-center px-4 py-4">
+<footer className="w-full bg-gray-100 text-center px-4 py-2">
   <div className="max-w-screen-lg mx-auto">
     <span className="block text-sm text-gray-500">
       © 2025 Abdiaziz. All Rights Reserved. Built with <span role="img" aria-label="heart">❤️</span>
